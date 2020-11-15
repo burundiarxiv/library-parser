77d1c34a0a70e422798047121b5af7b2ec178cbd
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-11-15 01:54:13 +0100

    Merge all chapters in one file

--- app.rb
+++ app.rb
@@ -4,3 +4,11 @@ Parser::TAGS.keys.each do |chapter|
   parser = Parser.new("data/#{chapter}.txt")
   parser.run
 end
+
+library = []
+Dir.glob('export/chap-*.json') do |f|
+  file = File.read(f)
+  library.concat JSON.parse(file)
+end
+
+File.open('export/library.json', 'w') { |file| file.write(library.to_json) }
